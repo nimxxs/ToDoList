fc918e16af3e449d8686b7e1ef00b60c46b6a5e7
bigdata <tnals2199@naver.com>
2024-02-14 21:52:23 +0900

    할일 체크 & 삭제 동작 구현

--- toDo.js
+++ toDo.js
@@ -5,8 +5,13 @@ let taskList = [];
 addButton.addEventListener("click", addTask);
 
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    // let taskContent = taskInput.value;
+    let task = {
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false,
+    };
+    taskList.push(task);
     console.log(taskList);
     render();
 }
@@ -14,14 +19,54 @@ function addTask() {
 function render() {
     let resultHTML = '';
     for(let i=0; i < taskList.length; i++) {
-        resultHTML += `<div class="task">
-        <div>${taskList[i]}</div>
-        <div>
-            <button>Check</button>
-            <button>Delete</button>
-        </div>
-    </div>`;
+        const taskClass = taskList[i].isComplete ? "task taskBackground" : "task";
+
+        if (taskList[i].isComplete == true) {
+            resultHTML += `<div class="${taskClass}">
+            <div class="taskDone">${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">
+                    <i class="fas fa-undo"></i>
+                </button>
+                <button onclick="deleteTask('${taskList[i].id}')">
+                    <i class="far fa-window-close"></i>
+                </button>
+            </div>
+        </div>`;
+        } else {
+                resultHTML += `<div class="${taskClass}">
+            <div>${taskList[i].taskContent}</div>
+            <div>
+                <button onclick="toggleComplete('${taskList[i].id}')">
+                    <i class="fas fa-check"></i>
+                </button>
+                <button onclick="deleteTask('${taskList[i].id}')">
+                    <i class="far fa-window-close"></i>
+                </button>
+            </div>
+         </div>`;
+        }
     }
 
     document.getElementById("taskBoard").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+    for(let i=0; i<taskList.length; i++){
+        if(taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render()
+    console.log(taskList);
+}
+
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function deleteTask(id) {
+    taskList = taskList.filter(task => task.id !== id);
+    render(); 
 }
